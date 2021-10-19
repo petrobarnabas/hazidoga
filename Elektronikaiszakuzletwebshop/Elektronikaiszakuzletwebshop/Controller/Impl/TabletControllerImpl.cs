635e1b3a8a680b2ad607e2465b7a2633f038604b
domvarga <Varga.Dominik@njit.hu>
2021-10-19 19:40:16 +0200

    Tablet impl

---
+++ Elektronikaiszakuzletwebshop/Elektronikaiszakuzletwebshop/Controller/Impl/TabletControllerImpl.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Elektronikaiszakuzletwebshop.Service;
+using Elektronikaiszakuzletwebshop.Entity;
+
+namespace Elektronikaiszakuzletwebshop.Controller.Impl
+{
+    [Route("tabletek")]
+    public class TabletControllerImpl : AbstractControllerBase<Tablet>
+    {
+        private readonly IGenericService<Tablet> tabletService;
+        public TabletControllerImpl(IGenericService<Tablet> tabletService)
+        {
+            this.tabletService = tabletService;
+        }
+        public override void Delete(int id)
+        {
+            tabletService.Delete(id);
+        }
+
+        public override List<Tablet> GetAll()
+        {
+            return tabletService.GetAll();
+        }
+
+        public override Tablet GetById(int id)
+        {
+            return tabletService.GetById(id);
+        }
+
+        public override Tablet Save(Tablet inputData)
+        {
+            return tabletService.Save(inputData);
+        }
+
+        public override Tablet Update(int id, Tablet updateData)
+        {
+            return tabletService.Update(id, updateData);
+        }
+    }
+}
