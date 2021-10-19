7ce4a5c33612e246bbf00e4480b9f62dcb6347d9
domvarga <Varga.Dominik@njit.hu>
2021-10-19 16:28:06 +0200

    Controller

---
+++ Elektronikaiszakuzletwebshop/Elektronikaiszakuzletwebshop/Controller/Impl/LaptopControllerImpl.cs
@@ -0,0 +1,44 @@
+﻿using ElektronikaiSzakuzletWebShopV2.Entity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ElektronikaiSzakuzletWebShopV2.Service;
+
+namespace ElektronikaiSzakuzletWebShopV2.Controller.Impl
+{
+    [Route("laptopok")]
+    public class LaptopControllerImpl : LaptopControllerBase<Laptop>
+    {
+        private readonly IGenericService<Laptop> laptopService;
+        public LaptopControllerImpl(IGenericService<Laptop> laptopService)
+        {
+            this.laptopService = laptopService;
+        }
+        public override void Delete(int id)
+        {
+            laptopService.Delete(id);
+        }
+
+        public override List<Laptop> GetAll()
+        {
+            return laptopService.GetAll();
+        }
+
+        public override Laptop GetById(int id)
+        {
+            return laptopService.GetById(id);
+        }
+
+        public override Laptop Save(Laptop inputData)
+        {
+            return laptopService.Save(inputData);
+        }
+
+        public override Laptop Update(int id, Laptop updateData)
+        {
+            return laptopService.Update(id, updateData);
+        }
+    }
+}
