7edd92a5fe6aa471b81955bff0ce826498aecd27
domvarga <Varga.Dominik@njit.hu>
2021-10-19 19:50:28 +0200

    repos

--- Elektronikaiszakuzletwebshop/Elektronikaiszakuzletwebshop/Repository/OrderRepository.cs
+++ Elektronikaiszakuzletwebshop/Elektronikaiszakuzletwebshop/Repository/OrderRepository.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿using System.Collections.Generic;
+using Elektronikaiszakuzletwebshop.Entity;
 namespace Elektronikaiszakuzletwebshop.Repository
 {
     public class OrderRepository
     {
+        private List<Order> orders = new List<Order>();
+        private int nextIncremantalId = 0;
+        public List<Order> Orders { get => orders; }
+        public int NextIncrementalId { get => ++nextIncremantalId; }
     }
 }
--- Elektronikaiszakuzletwebshop/Elektronikaiszakuzletwebshop/Repository/TabletRepository.cs
+++ Elektronikaiszakuzletwebshop/Elektronikaiszakuzletwebshop/Repository/TabletRepository.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿using System.Collections.Generic;
+using Elektronikaiszakuzletwebshop.Entity;
 namespace Elektronikaiszakuzletwebshop.Repository
 {
     public class TabletRepository
     {
+        private List<Tablet> tablets = new List<Tablet>();
+        private int nextIncremantalId = 0;
+        public List<Tablet> Tablets { get => tablets; }
+        public int NextIncrementalId { get => ++nextIncremantalId; }
     }
 }
